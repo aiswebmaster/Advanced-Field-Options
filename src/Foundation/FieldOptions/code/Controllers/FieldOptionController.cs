83ff81082bc24405b7c392c6535b9d0bd8f91147
aiswebmaster <dylan@allisolutions.com>
2017-06-07 00:00:57 -0400

    Initial Controller/Repo Setup

---
+++ src/Foundation/FieldOptions/code/Controllers/FieldOptionController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sitecore.Foundation.FieldOptions.Controllers
+{
+    public class FieldOptionController
+    {
+        public ActionResult OptionsView()
+        {
+            return View(null);
+        }
+    }
+}
